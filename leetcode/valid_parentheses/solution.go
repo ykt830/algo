63b12922e0e6508d1a459e59d1084dc0db98712c
yangxu <ykt.self@outlook.com>
2023-08-17 12:54:12 +0800

    feat: solve valid_parentheses

---
+++ leetcode/valid_parentheses/solution.go
@@ -0,0 +1,64 @@
+package valid_parentheses
+
+/*
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+每个右括号都有一个对应的相同类型的左括号。
+
+
+示例 1：
+
+输入：s = "()"
+输出：true
+示例 2：
+
+输入：s = "()[]{}"
+输出：true
+示例 3：
+
+输入：s = "(]"
+输出：false
+
+
+提示：
+1 <= s.length <= 104
+s 仅由括号 '()[]{}' 组成
+*/
+
+func isValid(s string) bool {
+	stack := make([]int32, 0)
+	for _, c := range s {
+		var top int32
+		if len(stack) > 0 {
+			top = stack[len(stack)-1]
+		}
+		switch c {
+		case ')':
+			if top == '(' {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		case '}':
+			if top == '{' {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		case ']':
+			if top == '[' {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		default:
+			stack = append(stack, c)
+		}
+	}
+
+	return len(stack) == 0
+}
