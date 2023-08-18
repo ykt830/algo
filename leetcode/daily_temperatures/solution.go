183fc9b3c5c35561b35ffdac0229cf9974e0fe62
yangxu <ykt.self@outlook.com>
2023-08-18 13:41:11 +0800

    feat: solve daily_temperatures

---
+++ leetcode/daily_temperatures/solution.go
@@ -0,0 +1,46 @@
+package daily_temperatures
+
+/*
+给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+
+示例 1:
+
+输入: temperatures = [73,74,75,71,69,72,76,73]
+输出: [1,1,4,2,1,1,0,0]
+示例 2:
+
+输入: temperatures = [30,40,50,60]
+输出: [1,1,1,0]
+示例 3:
+
+输入: temperatures = [30,60,90]
+输出: [1,1,0]
+
+
+提示：
+
+1 <= temperatures.length <= 105
+30 <= temperatures[i] <= 100
+*/
+
+func dailyTemperatures(temperatures []int) []int {
+	stack, ret := make([][]int, 0), make([]int, len(temperatures))
+	for i := 0; i < len(temperatures); i++ {
+		temp := temperatures[i]
+		for len(stack) > 0 {
+			top := stack[len(stack)-1]
+			if temp > top[1] {
+				ret[top[0]] = i - top[0]
+				stack = stack[:len(stack)-1]
+			} else {
+				break
+			}
+		}
+		stack = append(stack, []int{i, temp})
+	}
+	for _, v := range stack {
+		ret[v[0]] = 0
+	}
+
+	return ret
+}
