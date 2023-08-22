d7134fc8f91d1c55a9a646d124c0f044438f1405
yangxu <ykt.self@outlook.com>
2023-08-22 13:43:57 +0800

    feat: solve decode_string

---
+++ leetcode/decode_string/solution_test.go
@@ -0,0 +1,29 @@
+package decode_string
+
+import "testing"
+
+func Test_decodeString(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test_1",
+			args: args{
+				s: "3[a]2[bc]",
+			},
+			want: "aaabcbc",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := decodeString(tt.args.s); got != tt.want {
+				t.Errorf("decodeString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
