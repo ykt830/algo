7a43c994afeddbae2d908e0bc4809ee03e6aa8db
yangxu <ykt.self@outlook.com>
2023-08-22 13:04:01 +0800

    feat: solve min_stack

---
+++ leetcode/min_stack/solution.go
@@ -0,0 +1,87 @@
+package min_stack
+
+import "math"
+
+/*
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+实现 MinStack 类:
+
+MinStack() 初始化堆栈对象。
+void push(int val) 将元素val推入堆栈。
+void pop() 删除堆栈顶部的元素。
+int top() 获取堆栈顶部的元素。
+int getMin() 获取堆栈中的最小元素。
+
+
+示例 1:
+
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+
+
+提示：
+
+-231 <= val <= 231 - 1
+pop、top 和 getMin 操作总是在 非空栈 上调用
+push, pop, top, and getMin最多被调用 3 * 104 次
+*/
+
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		stack:    []int{},
+		minStack: []int{},
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	min := math.MaxInt
+	if len(this.minStack) > 0 {
+		min = this.minStack[len(this.minStack)-1]
+	}
+	if min > val {
+		min = val
+	}
+
+	this.stack, this.minStack = append(this.stack, val), append(this.minStack, min)
+}
+
+func (this *MinStack) Pop() {
+	this.stack, this.minStack = this.stack[:len(this.stack)-1], this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
