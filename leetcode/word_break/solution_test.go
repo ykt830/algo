1fe70262e14123cc4d097ce6f54e415997cd70e6
yangxu <ykt.self@outlook.com>
2023-08-27 00:19:23 +0800

    faat: solve word_break

---
+++ leetcode/word_break/solution_test.go
@@ -0,0 +1,31 @@
+package word_break
+
+import "testing"
+
+func Test_wordBreak(t *testing.T) {
+	type args struct {
+		s        string
+		wordDict []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test_1",
+			args: args{
+				s:        "applepenapple",
+				wordDict: []string{"apple", "pen"},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := wordBreak(tt.args.s, tt.args.wordDict); got != tt.want {
+				t.Errorf("wordBreak() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
